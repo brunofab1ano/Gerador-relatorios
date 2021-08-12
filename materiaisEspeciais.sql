175eeceb0dbf505a6082911f64ba823b0ba2b2f9
brunofab1ano <88834513+brunofab1ano@users.noreply.github.com>
2021-08-12 09:22:58 -0300

    Create materiaisEspeciais.sql

---
+++ materiaisEspeciais.sql
@@ -0,0 +1,49 @@
+select
+    distinct
+    R.COD ||' - '||
+    (
+        select NOME
+        from TBMATMED
+        where TAB = R.TAB
+            and COD = R.COD
+    ) NOMEMAT,
+    R.CODREF ||' - '|| F.NOME NOMEREF,
+    F.DATA,
+    (
+        select COD_TUSS
+        from TBREFMEMTUSS Q
+        where Q.TAB_FCREFUND = F.TAB
+            and Q.COD_FCREFUND = F.COD
+            and Q.CLASSIF_FCREFUND = F.CLASSIF
+            and (DATA_VIGENCIA_INICIAL != '31.12.1899')
+            and (DATA_VIGENCIA_FINAL >= current_date)
+    ) COD_TUSS,
+    case
+        F.LABORA
+        when 'MATERIAL ESPECIAL'        then       'MATERIAL ESPECIAL COM AUTORIZAÇÃO'
+        when 'MATERIAL ESPECIAL SEM'    then       'MATERIAL ESPECIAL SEM AUTORIZAÇÃO'
+        when 'ORTESES'                  then       'ORTESES COM AUTORIZAÇÃO'
+        when 'ORTESES SEM AUTORIZACA'   then       'ORTESES SEM AUTORIZAÇÃO'
+        when 'PROTESES'                 then       'PROTESES COM AUTORIZAÇÃO'
+    end CLASSIFICACAO
+from TBREFUNI R
+    inner join FCREFUND F on t.TAB = f.TAB
+                        and t.classif = f.classif
+
+    inner join TBMATMED T on R.ID_TBMATMED = T.ID
+where t.TAB = 05
+    and t.CLASSIF = 1
+    and F.DATA in (
+        select max(FCREFUND.DATA)
+        from FCREFUND
+        where FCREFUND.COD = F.COD
+            and FCREFUND.TAB = F.TAB
+    )
+    and F.LABORA in (
+        'MATERIAL ESPECIAL',
+        'MATERIAL ESPECIAL SEM',
+        'ORTESES',
+        'ORTESES SEM AUTORIZACA',
+        'PROTESES'
+    )
+order by 5
