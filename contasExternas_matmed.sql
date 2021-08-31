61e9b9102e4f58614c65215de5581a116d5ce59e
brunofab1ano <88834513+brunofab1ano@users.noreply.github.com>
2021-08-31 13:36:02 -0300

    Update contasExternas_matmed.sql
    Ajuste Performance relatório

--- contasExternas_matmed.sql
+++ contasExternas_matmed.sql
@@ -1,86 +1,48 @@
-WITH cte AS (
+with CTE AS (
+
 SELECT DISTINCT
     C.COD,
     C.PACIENTE,
     C.CON || '-' || T.NOME CONVENIO,
-    (
-        SELECT AA.NOME
-        FROM TBCLINICA AA
-        WHERE AA.COD = E.CLINICA
-    ) CLINICA,
-    (
-        SELECT BB.NOME
-        FROM TBESPEC BB
-        WHERE BB.COD = E.ESPEC
-    ) ESPECIALIDADE,
-
+    cli.nome CLINICA,
+    ESP.nome ESPECIALIDADE ,
     e.nome_med medico,
 
-    --Lista Serviço Hospitalar Lançaddo dentro do faturamento
+    --Serviço Hospitalar faturamento
     (
         SELECT COALESCE(SUM(LLL.VALOR_C),0) TOTAL
-        FROM FCCTAEXT CCC
-            LEFT JOIN FCLANEXT LLL ON CCC.ID = LLL.ID_FCCTAEXT
-        WHERE CCC.REG_ATE = C.REG_ATE
-            AND (
-                (
-                    CCC.ANOPRO = C.ANOPRO
-                    AND CCC.MESPRO = C.MESPRO
-                )
-            )
-            AND CCC.TIPO_CTA = 'E'
-            AND LLL.TIPO = 1
-    ) SH,
-    
-        --Lista Serviço Profissional
+
+        FROM FCLANEXT LLL
+
+        WHERE lll.id_fcctaext = c.id
+        AND LLL.TIPO = 1
+    ) SH ,
+
+    --Serviço PROFISSIONAL
     (
-        SELECT COALESCE(SUM(LLL.VALOR_C),0)
-        FROM FCCTAEXT CCC
-            LEFT JOIN FCLANEXT LLL ON CCC.ID = LLL.ID_FCCTAEXT
-        WHERE CCC.REG_ATE = C.REG_ATE
-            AND (
-                (
-                    CCC.ANOPRO = C.ANOPRO
-                    AND CCC.MESPRO = C.MESPRO
-                )
-            )
-            AND CCC.TIPO_CTA = 'E'
-            AND LLL.TIPO = 2
+        SELECT COALESCE(SUM(LLL.VALOR_C),0) TOTAL
+        FROM FCLANEXT LLL
+        WHERE lll.id_fcctaext = c.id
+        AND LLL.TIPO = 2
     ) SP,
 
-        (
-        SELECT COALESCE(SUM(LLL.VALOR_C),0)
-        FROM FCCTAEXT CCC
-            LEFT JOIN FCLANEXT LLL ON CCC.ID = LLL.ID_FCCTAEXT
-        WHERE CCC.REG_ATE = C.REG_ATE
-            AND (
-                (
-                    CCC.ANOPRO = C.ANOPRO
-                    AND CCC.MESPRO = C.MESPRO
-                )
-            )
-            AND CCC.TIPO_CTA = 'E'
-            AND LLL.TIPO = 3
+    --RECURSO COMPLEMENTAR
+    (
+        SELECT COALESCE(SUM(LLL.VALOR_C),0) TOTAL
+        FROM FCLANEXT LLL
+        WHERE lll.id_fcctaext = c.id
+        AND LLL.TIPO = 3
     ) RC,
 
-         --Lista Recurso complementar
+    -- MAT/MED FATURAMENTO
     (
-        SELECT COALESCE(SUM(LLL.VALOR_C),0)
-        FROM FCCTAEXT CCC
-            LEFT JOIN FCLANEXT LLL ON CCC.ID = LLL.ID_FCCTAEXT
-        WHERE CCC.REG_ATE = C.REG_ATE
-            AND (
-                (
-                    CCC.ANOPRO = C.ANOPRO
-                    AND CCC.MESPRO = C.MESPRO
-                )
-            )
-            AND CCC.TIPO_CTA = 'E'
-            AND LLL.TIPO = 4
-    ) MM,
+        SELECT COALESCE(SUM(LLL.VALOR_C),0) TOTAL
+        FROM FCLANEXT LLL
+        WHERE lll.id_fcctaext = c.id
+        AND LLL.TIPO = 4
+    ) MM ,
 
-    --Calcular valor com base na saida do estoque (saidas - devoluções) * preço de tabela
-(SELECT
+    (SELECT
         SUM (
             (
             (L.QTDE) - COALESCE(
@@ -97,45 +59,38 @@ SELECT DISTINCT
             )
         ) * (
     SELECT VALOR1
-    FROM SPBRAVEN(5, L.ITEM, fc.data_emi, 1)
+    FROM SPBRAVEN(5, L.ITEM, C.data_emi, 1)
 ) * (CASE WHEN i.classif = 2 THEN (1.10) WHEN i.classif = 1 THEN (0.60) ELSE (1) END))
 
 
     FROM GECADSAI D
-        INNER JOIN RECADATE A ON D.PAC = A.REG
         INNER JOIN GELANSAI L ON D.DOC = L.DOC
                                 AND D.ANO = L.ANO
                                 AND D.MES = L.MES
         INNER JOIN GEITENS I ON L.ITEM = I.COD
-        INNER JOIN RICADPAC CP ON A.PRONT = CP.PRONT
-        INNER JOIN fcctaext fc ON a.reg = fc.cod
     WHERE L.CONSOL = 'T'
         AND D.CONV = c.con
         AND D.PAC = c.reg_ate
-        AND CHAR_LENGTH(D.PAC) > 6
-        AND A.PEXT = 'S'
 ) vl_MM
 
 
 
-
+    
 FROM FCCTAEXT C
-    LEFT JOIN FCLANEXT L ON C.ID = L.ID_FCCTAEXT
-    LEFT JOIN TBCONVEN T ON (T.COD = C.CON)
-    LEFT JOIN RECADATE E ON E.REG = C.REG_ATE
-WHERE --C.REG_ATE = 1640811 AND
+    --INNER JOIN FCLANEXT L ON C.ID = L.ID_FCCTAEXT
+    iNNER JOIN TBCONVEN T ON (T.COD = C.CON)
+    INNER JOIN RECADATE E ON E.REG = C.REG_ATE
+    INNER JOIN TBCLINICA CLI ON E.clinica = CLI.cod
+    INNER JOIN tbespec ESP ON E.espec = ESP.cod
+WHERE --c.reg_ate = 1640811 and
     C.IND_EMI = 'T'
-    AND (
-        (
-            C.ANOPRO = 2021
-            AND C.MESPRO = 05
-        )
-    )
-    AND C.TIPO_CTA = 'E'
+    AND C.ANOPRO = 2021
+    AND C.MESPRO = 05
+     AND C.TIPO_CTA = 'E'
     AND C.CON IN (12,85)
     AND E.CDC = 218
-)
 
+)
 
 SELECT
 cod,
